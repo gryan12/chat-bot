cc4b93272181cb0489ad2af5a37837c68f4650f8
gryan12 <g.aj.ryan@live.co.uk>
2019-04-09 21:24:44 +0100

    vocab etc

--- sample.py
+++ sample.py
@@ -1,6 +1,7 @@
 import pickle
 import numpy as np
-
+from keras.preprocessing.sequence import pad_sequences
+from keras.preprocessing.text import Tokenizer
 
 #data
 
@@ -16,3 +17,24 @@ print(type(train_data))
 print(len(test_data))
 print(len(train_data))
 print(train_data[0])
+print(' '.join(train_data[0][0]))
+print(' '.join(test_data[1][1]))
+vocab = set()
+all_data = test_data + train_data
+for story, question , answer in all_data:
+    vocab = vocab.union(set(story))
+    vocab = vocab.union(set(question))
+
+vocab.add('no')
+vocab.add('yes')
+print(vocab)
+vocab_len = len(vocab) + 1
+max_story_len = max([len(data[0]) for data in all_data])
+
+tokenizer = Tokenizer(filters=[])
+tokenizer.fit_on_texts(vocab)
+
+tokenizer.word_index
+
+
+
